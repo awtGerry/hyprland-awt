5cad4e035d7ec97c2315490c55e8716cd4de7377
Victor Rodriguez <84054959+awtGerry@users.noreply.github.com>
2023-06-24 13:32:17 -0600

    install

--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
 echo "Installing wayland & hyprland..."
-# paru -S hyprland-git hyprpicker-git waybar-hyprland-git dunst nwg-look wf-recored wlogout wlsunset
-# nwg-look wf-recored
-# paru -S hyprland-git hyprpicker-git waybar-hyprland-git dunst wlogout wlsunset
+paru -S hyprland-git hyprpicker-git waybar-hyprland-git dunst wlogout wlsunset
 
 echo "Installing dependencies..."
-# sway-bg xwaylandvideobridge-cursor-mode-2-git 
-# paru -S colord ffmpegthumbnailer gnome-keyring grimblast-git gtk-engine-murrine imagemagick \
-#	kvantum pamixer playerctl pulsemixer polkit-kde-agent qt5-quickcontrols qt5-quickcontrols2 \
-#	qt5-wayland qt6-wayland ttf-font-awesome tumbler ttf-jetbrains-mono ttf-icomoon-feather \
-#	xdg-desktop-portal-hyprland-git xdotool cliphist qt5-imageformats qt5ct
+paru -S colord ffmpegthumbnailer gnome-keyring grimblast-git gtk-engine-murrine imagemagick \
+kvantum pamixer playerctl pulsemixer polkit-kde-agent qt5-quickcontrols qt5-quickcontrols2 \
+qt5-wayland qt6-wayland ttf-font-awesome tumbler ttf-jetbrains-mono ttf-icomoon-feather \
+xdg-desktop-portal-hyprland-git xdotool cliphist qt5-imageformats qt5ct
 
 echo "Installing programs..."
-paru -S btop cava neofetch noise-suppression-for-voice rofi-lbonn-wayland-git rofi-emoji starship zsh viewnior
-paru -S brave-bin file-roller noto-fonts noto-fonts-cjl noto-fonts-emoji
-paru -S catppucin-gtk-theme papirus-icon-theme sddm-git swaylock-effects-git
-paru -S pipewire pipewire-alsa pipewire-audio pipewire-pulse pipewire-jack wireplumber gst-plugin-pipewire pavucontrol
+paru -S btop cava neofetch noise-suppression-for-voice rofi-lbonn-wayland-git rofi-emoji starship zsh viewnior \ # cli
+brave-bin file-roller noto-fonts noto-fonts-cjl noto-fonts-emoji \ # navegacion
+catppucin-gtk-theme papirus-icon-theme sddm-git swaylock-effects-git \ # temas
+pipewire pipewire-alsa pipewire-audio pipewire-pulse pipewire-jack wireplumber gst-plugin-pipewire pavucontrol \ # audio
